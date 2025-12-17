6e77ad8b4c632cd7772efa3f12ab042939004c45
Viktor Pro <146160822+ViktorPro1@users.noreply.github.com>
2025-12-17 09:57:18 +0200

    задеплоїв та вніс в сервіс ворк версію наступну

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 
-const CACHE_NAME = "webstart-cache-v10";
+const CACHE_NAME = "webstart-cache-v11";
 
 self.addEventListener("install", (event) => {
     console.log('[SW] Install');
