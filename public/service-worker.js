aa4fc61e96cdcb75bffb9b27ccb84155456ca6c9
Viktor Pro <146160822+ViktorPro1@users.noreply.github.com>
2025-12-28 12:20:11 +0200

    зменшив ,назву занадто довга (61 символ). Оптимальна довжина – 50-60 символів.В головному штмл

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 
 
-const CACHE_NAME = "webstart-cache-v49"; // Оновив версію для активації змін
+const CACHE_NAME = "webstart-cache-v50"; // Оновив версію для активації змін
 
 self.addEventListener("install", (event) => {
     console.log('[SW] Install');
