f18c87bc58844c05912c6043ee4f11cad3e34e59
Viktor Pro <146160822+ViktorPro1@users.noreply.github.com>
2026-01-02 10:42:57 +0200

    видалив зайві кольори

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,6 +1,6 @@
 
 
-const CACHE_NAME = "webstart-cache-v64"; // Оновив версію для активації змін
+const CACHE_NAME = "webstart-cache-v65"; // Оновив версію для активації змін
 
 self.addEventListener("install", (event) => {
     console.log('[SW] Install');
