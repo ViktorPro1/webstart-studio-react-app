fa09539d53068518f2c49c743818d7d680f5458c
Viktor Pro <146160822+ViktorPro1@users.noreply.github.com>
2026-01-05 15:26:08 +0200

    Змінив швидкістьбігучого рядка в головній сторінці, та відалив в модалці акцій закриваючий хрестик.

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,6 +1,6 @@
 
 
-const CACHE_NAME = "webstart-cache-v71"; // Оновив версію для активації змін
+const CACHE_NAME = "webstart-cache-v73"; // Оновив версію для активації змін
 
 self.addEventListener("install", (event) => {
     console.log('[SW] Install');
