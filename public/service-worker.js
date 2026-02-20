646284fcefddad74b4e8a0fc691e7591354e1504
Viktor Pro <victor.ozdoba@gmail.com>
2026-02-20 19:50:48 +0200

    обєднавдвігілки в одну маін,дебуловиправлено парубагівта додано нові навички

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 
 
-const CACHE_NAME = "webstart-cache-v79"; // Оновив версію для активації змін
+const CACHE_NAME = "webstart-cache-v80"; // Оновив версію для активації змін
 
 self.addEventListener("install", (event) => {
     console.log('[SW] Install');
