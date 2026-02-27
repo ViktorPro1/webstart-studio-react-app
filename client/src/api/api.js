738074099fd07152161cc3b018ea94388609845a
Viktor Pro <victor.ozdoba@gmail.com>
2026-02-27 20:33:21 +0200

    feat: update API URL to Railway production

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: import.meta.env.PROD
+        ? 'https://webstart-studio-react-app-production.up.railway.app/api'
+        : 'http://localhost:5000/api'
 });
 
 export default API;
